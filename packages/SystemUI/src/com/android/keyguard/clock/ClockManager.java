ed3280a86c153bf269768cd49acc7b61afbf9983
rohan <purohit.rohan@gmail.com>
2020-01-20 23:48:18 +0530

    Add Custom Spectrum Analog Clock
    Thanks to mohancm
    Thanks to ROGERdotT for thumbnail.
    Change-Id: I6cca8ce309edd40cd2072f557778c95a28933ded
    
    Change-Id: I712b473d0b93e6f83118ca7d88d58cc5581518cb
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- packages/SystemUI/src/com/android/keyguard/clock/ClockManager.java
+++ packages/SystemUI/src/com/android/keyguard/clock/ClockManager.java
@@ -162,6 +162,7 @@ public final class ClockManager {
         addBuiltinClock(() -> new MNMLBoxClockController(res, layoutInflater, colorExtractor));
         addBuiltinClock(() -> new MNMLMinimalClockController(res, layoutInflater, colorExtractor));
         addBuiltinClock(() -> new OronosClockController(res, layoutInflater, colorExtractor));
+        addBuiltinClock(() -> new SpectrumClockController(res, layoutInflater, colorExtractor));
 
         // Store the size of the display for generation of clock preview.
         DisplayMetrics dm = res.getDisplayMetrics();
